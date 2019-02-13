370a9d42904479ea780afec59d844adfa3101237
nicoleorzan <orzan.nicole@gmail.com>
2019-02-13 15:23:22 +0100

    Creating DateParser constructor and setting test class

---
+++ src/main/java/berlinclock/DateParser.java
@@ -0,0 +1,15 @@
+package berlinclock;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class DateParser {
+
+    private Calendar calendar;
+
+    public DateParser(Date date) {
+        calendar = GregorianCalendar.getInstance();
+        calendar.setTime(date);
+    }
+}
---
+++ src/test/java/berlinclock/DateParserTest.java
@@ -0,0 +1,9 @@
+package berlinclock;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DateParserTest {
+
+}
