33491652fb623f1fc5cef18a1460bc953a84bff0
nicoleorzan <orzan.nicole@gmail.com>
2019-02-14 16:31:12 +0100

    Passed first test of TimeDisplayer class, to get exact character from seconds value

--- src/main/java/berlinclock/TimeDisplayer.java
+++ src/main/java/berlinclock/TimeDisplayer.java
@@ -1,10 +1,18 @@
 package berlinclock;
 
+import java.util.Date;
+
 public class TimeDisplayer {
 
     private TimeParser timeParser;
 
-    public TimeDisplayer(TimeParser time) {
-        timeParser = time;
+    public TimeDisplayer(Date time) {
+        timeParser = new TimeParser(time);
+    }
+
+    public char computeSecondStatus(){
+        char second = 'O';
+        if (timeParser.getSeconds() == 1) second = 'R';
+        return second;
     }
 }
--- src/test/java/test/berlinclock/TimeDisplayerTest.java
+++ src/test/java/test/berlinclock/TimeDisplayerTest.java
@@ -1,10 +1,24 @@
 package test.berlinclock;
 
+import berlinclock.TimeDisplayer;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class TimeDisplayerTest {
 
+    @Test
+    public void  computeSecondStatusTest() throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        Date time = format.parse("12:41:32");
+        TimeDisplayer timedisplayer = new TimeDisplayer(time);
 
+        assertThat(timedisplayer.computeSecondStatus(), is(equalTo('O')));
+    }
 }
