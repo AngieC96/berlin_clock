414c1e94649e210a1e0940b3cbb0995d6403e299
Francesco Franchina <cescus92@gmail.com>
2019-02-15 17:11:24 +0100

    Implemented the function to compute output string of five-block hours

--- src/main/java/berlinclock/TimeDisplayer.java
+++ src/main/java/berlinclock/TimeDisplayer.java
@@ -7,4 +7,12 @@ public class TimeDisplayer {
     private StatusComputer statusComputer;
 
     public TimeDisplayer(Date time){ statusComputer = new StatusComputer(time); }
+
+    public String getFiveBlockHourString() {
+        String template = "   %c    %c     %c    %c   ";
+        char[] status = statusComputer.computeFiveBlockHourStatus();
+        String output = String.format(template, status[0], status[1], status[2], status[3]);
+
+        return output;
+    }
 }
--- src/test/java/test/berlinclock/TimeDisplayerTest.java
+++ src/test/java/test/berlinclock/TimeDisplayerTest.java
@@ -1,7 +1,28 @@
 package test.berlinclock;
 
+import berlinclock.StatusComputer;
+import berlinclock.TimeDisplayer;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.*;
 
 public class TimeDisplayerTest {
+
+    @Test
+    public void getFiveBlockHourStringTest() throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        Date time = format.parse("12:41:32");
+        TimeDisplayer timeDisplayer = new TimeDisplayer(time);
+
+        String expected = "   R    R     O    O   ";
+
+        assertThat(timeDisplayer.getFiveBlockHourString(), is(equalTo(expected)));
+    }
 
 }
