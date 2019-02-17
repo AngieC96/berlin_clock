a74bec495ab980063cada9abb557c7a76b1c71cf
Francesco Franchina <cescus92@gmail.com>
2019-02-17 19:13:08 +0100

    Renamed a variable to fit the current scope inside StatusComputerTest

--- src/test/java/test/berlinclock/StatusComputerTest.java
+++ src/test/java/test/berlinclock/StatusComputerTest.java
@@ -15,48 +15,48 @@ public class StatusComputerTest {
     @Test
     public void computeSecondStatusTest() {
         LocalTime time = LocalTime.of(12, 35, 12);
-        StatusComputer timeDisplayer = new StatusComputer(time);
+        StatusComputer statusComputer = new StatusComputer(time);
 
-        assertThat(timeDisplayer.computeSecondStatus(), is(equalTo('R')));
+        assertThat(statusComputer.computeSecondStatus(), is(equalTo('R')));
     }
 
     @Test
     public void computeFiveBlockHourStatusTest() {
         LocalTime time = LocalTime.of(17, 41, 32);
-        StatusComputer timeDisplayer = new StatusComputer(time);
+        StatusComputer statusComputer = new StatusComputer(time);
 
         char[] expected = {'R', 'R', 'R', 'O'};
 
-        assertArrayEquals(timeDisplayer.computeFiveBlockHourStatus(), expected);
+        assertArrayEquals(statusComputer.computeFiveBlockHourStatus(), expected);
     }
 
     @Test
     public void computeSingleBlockHourStatusTest() {
         LocalTime time = LocalTime.of(17, 42, 32);
-        StatusComputer timeDisplayer = new StatusComputer(time);
+        StatusComputer statusComputer = new StatusComputer(time);
 
         char[] expected = {'R', 'R', 'O', 'O'};
 
-        assertArrayEquals(timeDisplayer.computeSingleBlockHourStatus(), expected);
+        assertArrayEquals(statusComputer.computeSingleBlockHourStatus(), expected);
     }
 
     @Test
     public void computeFiveBlockMinuteStatusTest() {
         LocalTime time = LocalTime.of(17, 47, 32);
-        StatusComputer timeDisplayer = new StatusComputer(time);
+        StatusComputer statusComputer = new StatusComputer(time);
 
         char[] expected = {'Y', 'Y', 'R', 'Y', 'Y', 'R', 'Y', 'Y', 'R', 'O', 'O'};
 
-        assertArrayEquals(timeDisplayer.computeFiveBlockMinuteStatus(), expected);
+        assertArrayEquals(statusComputer.computeFiveBlockMinuteStatus(), expected);
     }
 
     @Test
     public void computeSingleBlockMinuteStatusTest() {
         LocalTime time = LocalTime.of(17, 7, 32);
-        StatusComputer timeDisplayer = new StatusComputer(time);
+        StatusComputer statusComputer = new StatusComputer(time);
 
         char[] expected = {'Y', 'Y', 'O', 'O'};
 
-        assertArrayEquals(timeDisplayer.computeSingleBlockMinuteStatus(), expected);
+        assertArrayEquals(statusComputer.computeSingleBlockMinuteStatus(), expected);
     }
 }
